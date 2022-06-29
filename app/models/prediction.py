795a3314b5b5e8ff0d3c11aee8ce7ef348401de8
Bekhruz Nutfilloyev <yoshlikmedia@gmail.com>
2022-06-29 12:31:47 +0500

    Getting request models

--- app/models/prediction.py
+++ app/models/prediction.py
@@ -1,10 +1,21 @@
-from typing import Optional
-
 from pydantic import BaseModel
 
 
+class GetPredictionRequest(BaseModel):
+    age: int
+    gender: int
+    height: float
+    weight: float
+    body_fat_percentage: float
+    diastolic: float
+    systolonic: float
+    gripForce: float
+    sit_and_bend_forward_cm: float
+    sit_ups_count: float
+    broad_jumps: float
+
 class MachineLearningResponse(BaseModel):
-    prediction: float
+    prediction: int
 
 
 class HealthResponse(BaseModel):
